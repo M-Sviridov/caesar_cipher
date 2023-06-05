4209581c6aa179c0ae06ac776bd46c58ea568153
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-06-05 13:43:42 +1000

    Refactor code to use class

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-def caesar_cipher(string, shift, result = '')
-  string.each_char do |char|
-    base = char.ord < 91 ? 65 : 97
+# class representing the caesar cipher encryption tecnique
+class CaesarCipher
+  def translate(string, shift, result = '')
+    string.each_char do |char|
+      base = char.ord < 91 ? 65 : 97
 
-    if char.ord.between?(65, 90) || char.ord.between?(97, 122)
-      shifted = (((char.ord - base) + shift) % 26) + base
-      result += shifted.chr
-    else
-      result += char
+      if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+        shifted = (((char.ord - base) + shift) % 26) + base
+        result += shifted.chr
+      else
+        result += char
+      end
     end
+    result
   end
-  result
 end
-
-puts caesar_cipher('What a string!', 5)
